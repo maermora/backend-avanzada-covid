81caccb0b6b9cedab8c146a7bebb1e83fc2e5730
Mauricio <maermora@gmail.com>
2020-07-20 21:23:22 -0500

    creation of parameters

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -13,12 +13,13 @@ const JWT_SECRETKEY = 'asdfjeirjkdf';
 
 const login = async(req, res) => {
     const {user, password} = req.body;
-    const auxuser = await pool.query('select contrasena from usuarios where usuario = $1', [user]);
+    const auxuser = await pool.query('select usuario, email, contrasena from usuarios where usuario = $1', [user]);
     if(!auxuser || auxuser.rows[0].contrasena !== password ) return res.status(403).send('Usuario o contraseña incorrectos');
     console.log('Bienvenido');
     const token = jwt.sign(user, JWT_SECRETKEY);
     res.send({
-        signed_user: user,
+        login_user: auxuser.rows[0].usuario,
+        login_email: auxuser.rows[0].email,
         token: token
     });
 }
@@ -28,29 +29,51 @@ const signup = async(req, res) => {
     const token = jwt.sign(user, JWT_SECRETKEY);
     res.send({
         signed_user: user,
+        signed_email: email,
         token: token
     })
     
 }
 const getPacientes = async (req, res) => {
+    if (req.query.sexo){
+        const pacientes = await pool.query('select * from pacientes where sexo = $1', [req.query.sexo]);
+        return res.json(pacientes.rows);
+    }
+    if (req.query.edad){
+        if (req.query.edad == 'infancia'){
+            const minimo = 0;
+            const maximo = 11;
+            const pacientes = await pool.query('select * from pacientes where edad >= $1 and edad <= $2', [minimo, maximo]);
+            return res.json(pacientes.rows);
+        }
+        if (req.query.edad == 'juventud'){
+            const minimo = 12;
+            const maximo = 26;
+            const pacientes = await pool.query('select * from pacientes where edad >= $1 and edad <= $2', [minimo, maximo]);
+            return res.json(pacientes.rows);
+        }
+        if (req.query.edad == 'adultez'){
+            const minimo = 27;
+            const maximo = 59;
+            const pacientes = await pool.query('select * from pacientes where edad >= $1 and edad <= $2', [minimo, maximo]);
+            return res.json(pacientes.rows);
+        }
+        if (req.query.edad == 'vejez'){
+            const minimo = 60;
+            const pacientes = await pool.query('select * from pacientes where edad >= $1', [minimo]);
+            return res.json(pacientes.rows);
+        }      
+    }
+    if (req.query.estado){
+        const pacientes = await pool.query('select * from pacientes where estado = $1', [req.query.estado]);
+        return res.json(pacientes.rows);
+    }
     const response = await pool.query('select * from pacientes');
-    res.json(response.rows);
-}
-const getPacientesM = async (req, res) => {
-    const sexo = req.query.sexo;
-    const response = await pool.query('select * from pacientes where sexo = $1', [sexo]);
-    res.json(response);
-}
-const getPacientesF = async (req, res) => {
-    const woman = 'F';
-    const response = await pool.query('select * from pacientes where sexo = $1', [woman]);
     res.json(response.rows);
 }
 
 module.exports = {
     getPacientes,
-    getPacientesM,
-    getPacientesF,
     login,
     signup
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 const { getPacientes,
-        getPacientesM,
-        getPacientesF,
         login, 
         signup} = require('../controllers/index.controller');
 
 router.get('/pacientes', getPacientes);
 router.post('/login', login);
 router.post('/signup',signup)
-router.get('/pacientesM', getPacientesM);
-router.get('/pacientes/mujeres', getPacientesF);
 
 module.exports = router;
